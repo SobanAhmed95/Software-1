74174a27750af3e47baf290604ffe58ac7de4780
SobanAhmed95 <sobanahmed9801@gmail.com>
2024-06-29 18:50:22 +0500

    Software

---
+++ src/Components/Input.tsx
@@ -0,0 +1,20 @@
+// import {Box , Input} from '@mui/material'
+// import * as React from 'react';
+// import TextField from '@mui/material/TextField';
+// import Stack from '@mui/material/Stack';
+// import Autocomplete from '@mui/material/Autocomplete';
+
+// export const Inputs = ({place , chlidren , onchange , label , type}:any) => {
+//     return (
+//         <Box>
+//            <TextField type={type} className='input' placeholder={place} label={label} onChange={onchange}/>
+
+//     <Stack spacing={2} sx={{ width: 300 }}>
+      
+      
+//     </Stack>
+
+// </Box>
+//     )
+
+// }
